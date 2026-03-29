4ffd16048c0e6f18956f7222c687c55596181b9b
Nagarajan E <132759664+nagarajan-elan@users.noreply.github.com>
2026-03-29 06:52:13 +0530

    CRUDL endpoints and basic shard selector

---
+++ hash_ring.py
@@ -0,0 +1,105 @@
+import hashlib
+import bisect
+from constants import DATABASE_SHARDS
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class ConsistentHashRing:
+    def __init__(self, nodes, shards):
+        self._engine_cache = {}
+        self.shard_map = {}
+        self.ring = []
+
+        for shard_id in shards:
+            for i in range(nodes):
+                key = f"{shard_id}-{i}"
+                h = self._hash(key)
+                self.ring.append(h)
+                self.shard_map[h] = shard_id
+
+        self.ring.sort()
+
+    def _hash(self, key: str) -> int:
+        return int(hashlib.sha256(key.encode()).hexdigest(), 16)
+
+    def get_engine(self, shard_id: int):
+        if shard_id not in self._engine_cache:
+            self._engine_cache[shard_id] = create_engine(DATABASE_SHARDS[shard_id])
+        return self._engine_cache[shard_id]
+
+    def get_shard_id(self, key: str) -> int:
+        k = self._hash(key)
+        clockwise_node_index = bisect.bisect_right(self.ring, k)
+        if clockwise_node_index == len(self.ring):
+            clockwise_node_index = 0
+
+        ring_key = self.ring[clockwise_node_index]
+        return self.shard_map[ring_key]
+
+    def get_session(self, key: str):
+        shard_id = self.get_shard_id(key)
+        engine = self.get_engine(shard_id)
+        Session = sessionmaker(bind=engine)
+        return Session()
+
+
+class ShardRouter:
+    def __init__(
+        self,
+    ):
+        self.ring: ConsistentHashRing = None
+        self.old_ring: ConsistentHashRing = None
+        self.state = None  # migrating, idle
+
+    def setup(self):
+        # handle adding or removing shard logic
+        self.ring = ConsistentHashRing()
+
+    def create_record(self, model, data):
+        try:
+            session = self.ring.get_session()
+            record = model(**data)
+            session.add(record)
+            session.commit()
+            session.refresh(record)  # get DB-generated values
+            return record
+        finally:
+            session.close()
+
+    def get_record(self, model, id):
+        try:
+            session = self.ring.get_session()
+            return session.query(model).filter(model.id == id).first()
+        finally:
+            session.close()
+
+    def update_record(self, model, record):
+        try:
+            session = self.ring.get_session()
+            id = record.pop("id")
+            session.query(model).filter(model.id == id).update(
+                **record, synchronize_session="fetch"
+            )
+            session.commit()
+        finally:
+            session.close()
+
+    def list_records(self, model):
+        try:
+            session = self.ring.get_session()
+            return session.query(model).all()
+        finally:
+            session.close()
+
+    def delete_record(self, model, id):
+        try:
+            session = self.ring.get_session()
+            record = session.query(model).filter(model.id == id).first()
+            if not record:
+                return False
+            session.delete(record)
+            session.commit()
+            return True
+        finally:
+            session.close()
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 from fastapi import FastAPI
-
+from hash_ring import ShardRouter
 from schemas import User
+from sqlalchemy_schemas import UserDB
 
 app = FastAPI()
+shard_router = ShardRouter()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/users/")
-def read_item(user: User):
-    return user.dict()
+def create_user(user: User):
+    shard_router.create_record(user)
+    return {"id": user.id}
+
+
+@app.get("/users")
+def list_users():
+    users = shard_router.list_records(UserDB)
+    return users
+
 
 @app.get("/users/{user_id}")
-def read_item(user_id: int):
-    return {"user_id": user_id}
+def get_user(user_id: str):
+    user = shard_router.get_record(UserDB, user_id)
+    return user
+
+
+@app.put("/users/{user_id}/")
+def update_user(user: User):
+    shard_router.update_record(UserDB, **user)
+    return {"id": user.id}
+
+
+@app.delete("/users/{user_id}/")
+def delete_user(user_id: str):
+    is_deleted = shard_router.delete_record(UserDB, user_id)
+    return {"status": is_deleted}
